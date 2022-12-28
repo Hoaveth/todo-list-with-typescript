58f2dfd8c967467d30d3b99c94d3ffa399b09eb2
Hoaveth <hoaveth.gaming@gmail.com>
2022-12-28 15:46:09 +0800

    Todo app init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField />
+    </div>
+  );
+};
+
+export default App;
